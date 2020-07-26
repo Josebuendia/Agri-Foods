7be7a4c7f6d538b4bbb29aa954d917c9d17d015f
Josebuendia <ftmoloney@hotmail.com>
2020-07-26 18:20:54 +0100

    Put a link to items in the header and changed the user_test.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+  def setup
+   # @user = User.new(email: 'liam@rock.com')
+   @user = users(:one)
+
+  end
+
+  test 'valid user' do
+    assert @user.valid?
+  end
+
+  test '#orders' do
+    assert_equal 1, @user.orders.size
+  end
+
+  # test 'invalid without name' do
+  #   @user. = nil
+  #   refute @user.valid?, 'saved user without a name'
+  #   assert_not_nil @user.errors[:name], 'no validation error for name present'
   # end
+
+  test 'invalid without email' do
+    @user.email = nil
+    refute @user.valid?
+    assert_not_nil @user.errors[:email]
+  end
 end
