730c4843919adabbc72418dc698afc9d69171134
Josebuendia <ftmoloney@hotmail.com>
2020-07-27 20:29:31 +0100

    Changed admin signin to seller signin.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,7 +2,8 @@ class UserController < ApplicationController
     def login
         session[:login] = 1
         session[:cart] = nil
-        flash[:notice] = "Admin Login sucessfull!!"
+        #Changed the line below to Seller Login sucessfull! from "Admin Login sucessfull!"
+        flash[:notice] = "Seller Login sucessfull!"
         redirect_to :controller => :items
         end
        
