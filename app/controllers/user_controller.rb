2373e82afe1de4eff16fa76ceb0060fed2ec5ed1
Josebuendia <ftmoloney@hotmail.com>
2020-08-01 22:56:24 +0100

    Fixed the indentation in the user controller.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,14 +5,14 @@ class UserController < ApplicationController
         #Changed the line below to Seller Login sucessfull! from "Admin Login sucessfull!"
         flash[:notice] = "Seller Login sucessfull!"
         redirect_to :controller => :items
-        end
+    end
 
     def logout
         session[:login] = nil
         session[:cart] = nil
         flash[:notice] = "You have been successfully logged out!!"
         redirect_to :controller => :items
-        end
+    end
 
         #iteration for farmers page
     def farmers
