c774fea68ebaca79087da4b1425893b65e397301
Josebuendia <ftmoloney@hotmail.com>
2020-08-02 16:10:01 +0100

    Resized some images and fixed the edit profile page.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
    before_action :configure_sign_up_params, only: [:create]
-   #before_action :configure_account_update_params, only: [:update]
+   before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -17,9 +17,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   
 
   # GET /resource/edit
-  # def edit
-  #   super
-  # end
+   def edit
+     super
+   end
 
   # PUT /resource
   # def update
@@ -55,9 +55,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+   def configure_account_update_params
+     devise_parameter_sanitizer.permit(:account_update, keys: [
+      :email, :password, :password_confirmation, :remember_me, :role, :farm_name, :farmers_picture, :about_farm, :farm_type, :county])
+   end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
