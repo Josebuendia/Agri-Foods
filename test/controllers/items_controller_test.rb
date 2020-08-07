235e6b44738d4bb589f349ca9149f45f6a20028d
Josebuendia <ftmoloney@hotmail.com>
2020-08-07 02:45:39 +0100

    Changed  the gemfile, some item folder HTML & the item test controller.

--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -17,7 +17,7 @@ class ItemsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create item" do
     assert_difference('Item.count') do
-      post items_url, params: { item: { category: @item.category, county: @item.county, description: @item.description, farm: @item.farm, image_url: @item.image_url, price: @item.price, shelf_life: @item.shelf_life, title: @item.title } }
+      post items_url, params: { item: { category: @item.category, county: @item.county, description: @item.description, farm: @item.farm, image: @item.image, price: @item.price, shelf_life: @item.shelf_life, title: @item.title } }
     end
 
     assert_redirected_to item_url(Item.last)
@@ -34,7 +34,7 @@ class ItemsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update item" do
-    patch item_url(@item), params: { item: { category: @item.category, county: @item.county, description: @item.description, farm: @item.farm, image_url: @item.image_url, price: @item.price, shelf_life: @item.shelf_life, title: @item.title } }
+    patch item_url(@item), params: { item: { category: @item.category, county: @item.county, description: @item.description, farm: @item.farm, id: @item.id, price: @item.price, shelf_life: @item.shelf_life, title: @item.title } }
     assert_redirected_to item_url(@item)
   end
 
