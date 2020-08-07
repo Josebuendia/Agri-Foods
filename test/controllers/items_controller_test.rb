52200b7f91e44313e85f7c87bb9654f977fa5298
Josebuendia <ftmoloney@hotmail.com>
2020-08-07 18:04:31 +0100

    Added a chat popup & changed some tests.

--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class ItemsControllerTest < ActionDispatch::IntegrationTest
+ # @image_image = fixture_path(Rails.root.join('test', 'fixtures', 'files', 'burger.jpg'),'image/jpg')
   setup do
+  #  @image_image = fixture_file_upload(Rails.root.join('test', 'fixtures', 'files', 'burger.jpg'),'image/jpg')
     @item = items(:one)
   end
 
@@ -17,7 +19,9 @@ class ItemsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create item" do
     assert_difference('Item.count') do
-      post items_url, params: { item: { category: @item.category, county: @item.county, description: @item.description, farm: @item.farm, image: @item.image, price: @item.price, shelf_life: @item.shelf_life, title: @item.title } }
+      post items_url, params: { item: { category: @item.category, county: @item.county, description: @item.description, farm: @item.farm,
+        # image_image: image_image, price: @item.price, shelf_life: @item.shelf_life, title: @item.title } }
+         image: @item.image, price: @item.price, shelf_life: @item.shelf_life, title: @item.title } }
     end
 
     assert_redirected_to item_url(Item.last)
@@ -34,13 +38,20 @@ class ItemsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update item" do
-    patch item_url(@item), params: { item: { category: @item.category, county: @item.county, description: @item.description, farm: @item.farm, id: @item.id, price: @item.price, shelf_life: @item.shelf_life, title: @item.title } }
+    patch item_url(@item), params: { item: { category: @item.category, county: @item.county, description: @item.description, farm: @item.farm, image_url: @item.image_url, price: @item.price, shelf_life: @item.shelf_life, title: @item.title } }
+    #patch item_url(@item), params:  id @item.id  
     assert_redirected_to item_url(@item)
   end
 
   test "should destroy item" do
     assert_difference('Item.count', -1) do
       delete item_url(@item)
+      #new code
+      #def delete_image_attachment
+        @image_image = ActiveStorage::Attachment.find(params[:id])
+        @image_image.purge
+        # 
+      
     end
 
     assert_redirected_to items_url
